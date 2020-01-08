1f4218bb0e6f3a77144fdb1a7d95782683bfcf78
Reece Dunham <me@rdil.rocks>
2020-01-08 16:16:17 +0000

    push template v1.0.0

---
+++ template/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default props => <p>Nothing too much here, so go ahead and get coding!</p>
+
+/*
+export default props => {
+    return (
+        <>
+            
+        </>
+    )
+}
+*/
